105b928168f38c5fdc2c3f6f0f9c662e97c0157e
우종현 <whdgus616@gmail.com>
2024-06-20 17:12:39 +0900

    에셋 추가, HUD UI 배치 완료

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,66 +1,130 @@
 using UnityEngine;
 using UnityEngine.UI;
-using DG.Tweening;
 
 public class UIManager : MonoBehaviour
 {
-    [SerializeField] private Button menuButton;
-    [SerializeField] private Button toggleInventoryButton;
-    [SerializeField] private Button gachaButton;
-    [SerializeField] private GameObject weaponInventoryScrollView;
+    // HUD 화면 버튼
+    [Header("HUDButton")]
+    [SerializeField] private Button MailBtn;
+    [SerializeField] private Button ContentsMenuBtn;
+    [SerializeField] private Button AmuletBtn;
 
-    private bool menuOpened = false;
+    // 퀵 메뉴 버튼
+    [Header("QuickMenuButton")]
+    [SerializeField] private Button AbilityBtn;
+    [SerializeField] private Button EquipmentBtn;
+    [SerializeField] private Button SkillBtn;
+    [SerializeField] private Button DungeonBtn;
+    [SerializeField] private Button ShopBtn;
+    [SerializeField] private Button HUDSummonBtn;
 
-    private void Start()
+    // 버튼을 누르면 나오게 될 패널
+    [Header("ContentsPanel")]
+    [SerializeField] private GameObject ContentsPanel;
+
+    // 퀵 메뉴에서 누르면 나오게 될 패널
+    [Header("QuickMenuPanel")]
+    [SerializeField] private GameObject AbilityPanel;
+    [SerializeField] private GameObject EquipmentPanel;
+    [SerializeField] private GameObject SkillPanel;
+    [SerializeField] private GameObject DungeonPanel;
+    [SerializeField] private GameObject ShopPanel;
+    [SerializeField] private GameObject SummonPanel;
+
+    // ContentsBtn을 누르면 나오게 되는 버튼
+    [Header("ContentsMenuButton")]
+    [SerializeField] private Button AttendanceBtn;
+    [SerializeField] private Button MissionBtn;
+    [SerializeField] private Button RankBtn;
+    [SerializeField] private Button BlessingBtn;
+    [SerializeField] private Button AttributeBtn;
+    [SerializeField] private Button AwakeBtn;
+    [SerializeField] private Button PassBtn;
+    [SerializeField] private Button InventoryBtn;
+    [SerializeField] private Button PowerSavingBtn;
+    [SerializeField] private Button FriendBtn;
+    [SerializeField] private Button GuildBtn;
+    [SerializeField] private Button SettingsBtn;
+
+    private void OnEnable()
     {
-        weaponInventoryScrollView.gameObject.SetActive(false);
-        toggleInventoryButton.gameObject.SetActive(false);
-        gachaButton.gameObject.SetActive(false);
+        MailBtn.onClick.AddListener(OnMailBtnClick);
+        ContentsMenuBtn.onClick.AddListener(OnContentsMenuBtnClick);
+        AmuletBtn.onClick.AddListener(OnAmuletBtnClick);
 
-        menuButton.onClick.AddListener(ToggleMenu);
-        toggleInventoryButton.onClick.AddListener(ToggleWeaponInventory);
+        AbilityBtn.onClick.AddListener(OnAbilityBtnClick);
+        EquipmentBtn.onClick.AddListener(OnEquipmentBtnClick);
+        SkillBtn.onClick.AddListener(OnSkillBtnClick);
+        DungeonBtn.onClick.AddListener(OnDungeonBtnClick);
+        ShopBtn.onClick.AddListener(OnShopBtnClick);
+        HUDSummonBtn.onClick.AddListener(OnHUDSummonBtnClick);
+
+        AttendanceBtn.onClick.AddListener(OnAttendanceBtnClick);
+        MissionBtn.onClick.AddListener(OnMissionBtnClick);
+        RankBtn.onClick.AddListener(OnRankBtnClick);
+        BlessingBtn.onClick.AddListener(OnBlessingBtnClick);
+        AttributeBtn.onClick.AddListener(OnAttributeBtnClick);
+        AwakeBtn.onClick.AddListener(OnAwakeBtnClick);
+        PassBtn.onClick.AddListener(OnPassBtnClick);
+        InventoryBtn.onClick.AddListener(OnInventoryBtnClick);
+        PowerSavingBtn.onClick.AddListener(OnPowerSavingBtnClick);
+        FriendBtn.onClick.AddListener(OnFriendBtnClick);
+        GuildBtn.onClick.AddListener(OnGuildBtnClick);
+        SettingsBtn.onClick.AddListener(OnSettingsBtnClick);
     }
 
-    private void ToggleMenu()
+    private void OnDisable()
     {
-        if (menuOpened)
-        {
-            CloseMenu();
-        }
-        else
-        {
-            OpenMenu();
-        }
+        MailBtn.onClick.RemoveListener(OnMailBtnClick);
+        ContentsMenuBtn.onClick.RemoveListener(OnContentsMenuBtnClick);
+        AmuletBtn.onClick.RemoveListener(OnAmuletBtnClick);
+
+        AbilityBtn.onClick.RemoveListener(OnAbilityBtnClick);
+        EquipmentBtn.onClick.RemoveListener(OnEquipmentBtnClick);
+        SkillBtn.onClick.RemoveListener(OnSkillBtnClick);
+        DungeonBtn.onClick.RemoveListener(OnDungeonBtnClick);
+        ShopBtn.onClick.RemoveListener(OnShopBtnClick);
+        HUDSummonBtn.onClick.RemoveListener(OnHUDSummonBtnClick);
+
+        AttendanceBtn.onClick.RemoveListener(OnAttendanceBtnClick);
+        MissionBtn.onClick.RemoveListener(OnMissionBtnClick);
+        RankBtn.onClick.RemoveListener(OnRankBtnClick);
+        BlessingBtn.onClick.RemoveListener(OnBlessingBtnClick);
+        AttributeBtn.onClick.RemoveListener(OnAttributeBtnClick);
+        AwakeBtn.onClick.RemoveListener(OnAwakeBtnClick);
+        PassBtn.onClick.RemoveListener(OnPassBtnClick);
+        InventoryBtn.onClick.RemoveListener(OnInventoryBtnClick);
+        PowerSavingBtn.onClick.RemoveListener(OnPowerSavingBtnClick);
+        FriendBtn.onClick.RemoveListener(OnFriendBtnClick);
+        GuildBtn.onClick.RemoveListener(OnGuildBtnClick);
+        SettingsBtn.onClick.RemoveListener(OnSettingsBtnClick);
     }
 
-    private void OpenMenu()
+    // 빈 함수들 선언
+    private void OnMailBtnClick()
     {
-        toggleInventoryButton.gameObject.SetActive(true);
-        gachaButton.gameObject.SetActive(true);
-
-        //Sequence sequence = DOTween.Sequence();
-        //sequence.Append(toggleInventoryButton.transform.DOMoveX(buttonContainer.position.x + 150, 0.3f).From(buttonContainer.position));
-        //sequence.Append(gachaButton.transform.DOMoveX(buttonContainer.position.x + 320, 0.3f).From(buttonContainer.position));
-
-        menuOpened = true;
+        // mail 
     }
+    private void OnContentsMenuBtnClick() { }
+    private void OnAmuletBtnClick() { }
 
-    private void CloseMenu()
-    {
-        toggleInventoryButton.gameObject.SetActive(false);
-        gachaButton.gameObject.SetActive(false);
-        //Sequence sequence = DOTween.Sequence();
-        //sequence.Append(gachaButton.transform.DOMoveX(buttonContainer.position.x, 0.3f));
-        //sequence.Append(toggleInventoryButton.transform.DOMoveX(buttonContainer.position.x, 0.3f)).OnComplete(() =>
-        //{
-            
-        //});
+    private void OnAbilityBtnClick() { }
+    private void OnEquipmentBtnClick() { }
+    private void OnSkillBtnClick() { }
+    private void OnDungeonBtnClick() { }
+    private void OnShopBtnClick() { }
+    private void OnHUDSummonBtnClick() { }
 
-        menuOpened = false;
-    }
-
-    private void ToggleWeaponInventory()
-    {
-        weaponInventoryScrollView.SetActive(!weaponInventoryScrollView.activeSelf);
-    }
+    private void OnAttendanceBtnClick() { }
+    private void OnMissionBtnClick() { }
+    private void OnRankBtnClick() { }
+    private void OnBlessingBtnClick() { }
+    private void OnAttributeBtnClick() { }
+    private void OnAwakeBtnClick() { }
+    private void OnPassBtnClick() { }
+    private void OnInventoryBtnClick() { }
+    private void OnPowerSavingBtnClick() { }
+    private void OnFriendBtnClick() { }
+    private void OnGuildBtnClick() { }
+    private void OnSettingsBtnClick() { }
 }
